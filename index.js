9e194b9c77501d20f19a095a40e9fb9ecd12e62f
Rasha92zee <rashahasoon923@gmail.com>
2024-10-28 23:15:21 +0530

    Added collision blocks to the game

--- index.js
+++ index.js
@@ -5,54 +5,55 @@ canvas.width = 1024;
 canvas.height = 576;
 
 const scaledCanvas = {
-    width: canvas.width / 4,
-    height: canvas.height / 4
+  width: canvas.width / 4,
+  height: canvas.height / 4,
+};
+
+const floorCollisions2D = [];
+for (let i = 0; i < floorCollisions.length; i += 36) {
+  floorCollisions2D.push(floorCollisions.slice(i, i + 36));
 }
+
+const collisionBlocks = [];
+floorCollisions2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 202) {
+      console.log("draw a block here!!!");
+      collisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 16,
+            y: y * 16,
+          },
+        })
+      );
+    }
+  });
+});
+
+const platformCollisions2D = [];
+for (let i = 0; i < platformCollisions.length; i += 36) {
+  platformCollisions2D.push(platformCollisions.slice(i, i + 36));
+}
+
+const platformCollisionBlocks = [];
+platformCollisions2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 202) {
+      console.log("draw a block here!!!");
+      platformCollisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 16,
+            y: y * 16,
+          },
+        })
+      );
+    }
+  });
+});
 
 const gravity = 0.5;
-
-class Sprite {
-  constructor({ position, imageSrc }) {
-    this.position = position;
-    this.image = new Image();
-    this.image.src = imageSrc;
-  }
-  draw() {
-    if (!this.image) return;
-    c.drawImage(this.image, this.position.x, this.position.y);
-  }
-  update() {
-    this.draw();
-  }
-}
-
-class Player {
-  constructor(position) {
-    this.position = position;
-    this.velocity = {
-      x: 0,
-      y: 1,
-    };
-    this.height = 100;
-  }
-  draw() {
-    c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, 100, this.height);
-  }
-
-  update() {
-    this.draw();
-
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-
-    if (this.position.y + this.height + this.velocity.y < canvas.height) {
-      this.velocity.y += gravity;
-    } else {
-      this.velocity.y = 0;
-    }
-  }
-}
 
 const player = new Player({
   x: 0,
@@ -78,18 +79,26 @@ const background = new Sprite({
     x: 0,
     y: 0,
   },
-  imageSrc: './img/background.png',
-})
+  imageSrc: "./img/background.png",
+});
 
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  
+
   c.save();
   c.scale(4, 4);
   c.translate(0, -background.image.height + scaledCanvas.height);
   background.update();
+  collisionBlocks.forEach(collisionBlock => {
+    collisionBlock.update()
+  })
+
+  platformCollisionBlocks.forEach((block) => {
+    block.update()
+  })
+
   c.restore();
 
   player.update();
